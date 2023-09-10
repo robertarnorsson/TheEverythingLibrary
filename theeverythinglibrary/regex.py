9f7a2a2942c58f03232fa888ade15a873ee6ccb3
Robert <rha006@gmail.com>
2023-09-10 23:55:10 +0200

    Fixes

--- theeverythinglibrary/regex.py
+++ theeverythinglibrary/regex.py
@@ -31,7 +31,7 @@ class TELRegex:
 
         self.common_patterns = {
             "email": r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b',
-            "url": r'https?://(?:www\.)?[A-Za-z0-9.-]+(?:\.[A-Za-z]{2,})+[\w/]*',
+            "url": r'(https?://)?(?:www\.)?[A-Za-z0-9.-]+(?:\.[A-Za-z]{2,})+[\w/]*',
             "date": r'\d{4}-\d{2}-\d{2}',
             "phone": r'\b(?:\+\d{1,2}\s?)?(?:\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}\b',
             "ip": r'\b(?:\d{1,3}\.){3}\d{1,3}\b',
@@ -47,6 +47,16 @@ class TELRegex:
         ---
         ### Description
         Retrieve a common regex pattern by name.\n
+        These are the available:
+        - email
+        - url
+        - date
+        - phone
+        - ip
+        - color
+        - word
+        - text
+        - number
         ---
         ### Arguments
             - `name`: The name of the common pattern to retrieve.\n
